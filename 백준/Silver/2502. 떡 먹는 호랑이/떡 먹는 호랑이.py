708f5d83434cad2047264e93cc2ec8ab2994e7f9
Hong Jiho <144138489+jih023@users.noreply.github.com>
2025-08-03 23:26:44 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 100 ms, Memory: 110576 KB -BaekjoonHub

---
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.py
@@ -0,0 +1,19 @@
+d, k = map(int, input().split())
+
+dp = [0 for _ in range(d)]
+dp[0], dp[1] = 1, 1
+
+while 1:
+    for i in range(2, d):
+        dp[i] = dp[i-1] + dp[i-2]
+    
+    if dp[d-1] == k:
+        break
+    elif dp[d-1] < k:
+        dp[1] += 1
+    else:
+        dp[0] += 1
+        dp[1] = dp[0]
+
+print(dp[0])
+print(dp[1])
